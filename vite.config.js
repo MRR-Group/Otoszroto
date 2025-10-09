15ff94799dc98a4b8f1fea8e499422ae2c880554
Kacper Walenga <kacper.walenga@studenci.collegiumwitelona.pl>
2025-10-10 00:04:13 +0200

    fix code style

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,42 @@
-import { defineConfig, loadEnv } from "vite";
-import vue from "@vitejs/plugin-vue";
-import laravel from "laravel-vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import laravel from 'laravel-vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 
 export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
   return defineConfig({
     build: {
-      outDir: "./public/build/",
+      outDir: './public/build/',
     },
     server: {
       host: true,
       port: 5173,
       strictPort: true,
-      origin: "http://" + process.env.VITE_DEV_SERVER_DOCKER_HOST_NAME,
+      origin: 'http://' + process.env.VITE_DEV_SERVER_DOCKER_HOST_NAME,
       cors: true, // Allow any origin
       watch: {
         ignored: [
-          "**/.idea/**",
-          "**/app/**",
-          "**/tests/**",
-          "**/bootstrap/**",
-          "**/public/**",
-          "**/vendor/**",
-          "**/storage/**",
-          "**/node_modules/**",
+          '**/.idea/**',
+          '**/app/**',
+          '**/tests/**',
+          '**/bootstrap/**',
+          '**/public/**',
+          '**/vendor/**',
+          '**/storage/**',
+          '**/node_modules/**',
         ],
       },
     },
     resolve: {
       alias: {
-        "@": "/resources/js",
+        '@': '/resources/js',
       },
     },
     plugins: [
       laravel({
-        input: "resources/js/app.ts",
+        input: 'resources/js/app.ts',
         refresh: true,
       }),
       vue({
@@ -49,5 +49,5 @@ export default ({ mode }) => {
       }),
       tailwindcss(),
     ],
-  });
-};
+  })
+}
